63e0356b78b9eda7db96da9ee97d219657f8a047
Nicolas Fortun <nicolas.fortun@etudiant.univ-rennes1.fr>
2017-09-15 08:46:18 +0200

    Modification du main et du logger

--- Projet/src/main/java/Server/Server.java
+++ Projet/src/main/java/Server/Server.java
@@ -1,21 +1,51 @@
 package Server;
 
+import Server.Sport;
+import Server.User;
+import Server.WeatherCondition;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
 
-    public static void main(String args[]){
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("dev");
         EntityManager manager = factory.createEntityManager();
 
         EntityTransaction tx = manager.getTransaction();
         tx.begin();
         try {
+            Sport bowling = new Sport();
+            bowling.setName("bowling");
+            bowling.setLevel("hardcore");
 
+            User jean = new User();
+            User paul = new User();
+            User jacques = new User();
 
+            jean.setLogin("jeannotlebo");
+            paul.setLogin("poildecarrot");
+            jacques.setLogin("jacadiadit");
+            List<User> list= new ArrayList<User>();
+            list.add(jean);
+            list.add(jacques);
+            list.add(paul);
+
+            bowling.setUsers(list);
+
+            manager.persist(jean);
+            manager.persist(paul);
+            manager.persist(jacques);
+            manager.persist(bowling);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -24,6 +54,6 @@ public class Server {
 
         manager.close();
         factory.close();
-
     }
+
 }
--- Projet/src/main/java/Server/User.java
+++ Projet/src/main/java/Server/User.java
@@ -2,6 +2,7 @@ package Server;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.util.List;

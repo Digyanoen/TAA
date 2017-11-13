a9772f1b0aebc539311e753fa2fa72546853b1cb
Nicolas Fortun <nicolas.fortun@etudiant.univ-rennes1.fr>
2017-11-13 14:49:44 +0100

    Schedule appel weather

--- Remise Projet/src/main/java/fr/istic/taa/Server/SampleDataJpaApplication.java
+++ Remise Projet/src/main/java/fr/istic/taa/Server/SampleDataJpaApplication.java
@@ -19,6 +19,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class SampleDataJpaApplication implements ApplicationRunner{
@@ -33,4 +35,5 @@ public class SampleDataJpaApplication implements ApplicationRunner{
         System.out.println("test");
 
     }
+
 }

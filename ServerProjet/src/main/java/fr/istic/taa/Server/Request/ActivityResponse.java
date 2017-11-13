f985cb0c02ce77c0e64c1942701b8bbf0e44a5d3
Julien Laurent <digyanoen@orange.fr>
2017-11-13 23:10:13 +0100

    Modification POJO

--- ServerProjet/src/main/java/fr/istic/taa/Server/Request/ActivityResponse.java
+++ ServerProjet/src/main/java/fr/istic/taa/Server/Request/ActivityResponse.java
@@ -1,6 +1,7 @@
 package fr.istic.taa.Server.Request;
 
 import fr.istic.taa.Server.model.WeatherCondition;
+import fr.istic.taa.Server.model.WeatherEnum;
 
 public class ActivityResponse {
     private int id;
@@ -9,7 +10,17 @@ public class ActivityResponse {
 
     private String city;
 
-    private WeatherCondition condition;
+    private WeatherEnum condition_name;
+
+    private int condition_strength;
+
+    public ActivityResponse(int id, String name, String city, WeatherEnum condition_name, int condition_strength) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.condition_name = condition_name;
+        this.condition_strength = condition_strength;
+    }
 
     public int getId() {
         return id;
@@ -35,11 +46,19 @@ public class ActivityResponse {
         this.city = city;
     }
 
-    public WeatherCondition getCondition() {
-        return condition;
+    public WeatherEnum getCondition_name() {
+        return condition_name;
     }
 
-    public void setCondition(WeatherCondition condition) {
-        this.condition = condition;
+    public void setCondition_name(WeatherEnum condition_name) {
+        this.condition_name = condition_name;
+    }
+
+    public int getCondition_strength() {
+        return condition_strength;
+    }
+
+    public void setCondition_strength(int condition_strength) {
+        this.condition_strength = condition_strength;
     }
 }
